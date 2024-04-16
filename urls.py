43afc6c4cf46f177634ea5330bc48ff35f3448f5
AmianAmel <121299958+AmianAmel@users.noreply.github.com>
2024-04-16 12:08:14 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,24 @@
-from .models import Domain, Training
+"""
+URL configuration for mon_projet project.
 
-# Get the domain you want to add the training to
-domain = Domain.objects.get(pk=1)  # Replace domain_id with the actual ID
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin 
+from django.urls import path,include
 
-# Create a new training object
-new_training = Training(
-    domain=domain,  # Set the domain field
-    training_name="web development",
-    training_descrption="you will study the front-end  back-end  ",
-)
+urlpatterns = [  path('odros/', include('odros.urls')),
+path('admin/', admin.site.urls),
+   
+]
 
-# Save the new training
-new_training.save()
-# Create a new domain object
-new_domain= Domain(
-    domain=domain,  # Set the domain field
-    training_name="Artificiel intelligence",
-    training_descrption="it's a field which ntersting with maching learning     ",
-)
-
-# Save the new domain
-new_domain.save()
